7091ddd8425cd4e6c830f070c5e2e66c8dc4d013
Denys Sidorov <000scorpions000@gmail.com>
2020-12-02 06:18:03 +0200

    add server CRUD with graphql, add frontend's app with components

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const app = express();
+const mongoose = require('mongoose');
 
-const schema = require('../schema/schema');
+const schema = require('./schema/schema');
 
 const PORT = 3005;
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/graphql-db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  (err) => {
+    if (err) throw err;
+    console.log('Mongo connected!');
+  }
+);
 
 app.use('/graphql', graphqlHTTP({
   schema,
